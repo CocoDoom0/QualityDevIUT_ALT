ff24eedc38140f93063078e66f6baf392c5c9842
Remi.mhx <remimahieux700@gmail.com>
2023-09-15 16:30:02 +0200

    Exercices 3.1 à 3.3

--- Exercice 1/Classes/Media.cs
+++ Exercice 1/Classes/Media.cs
@@ -7,20 +7,20 @@ public class Media
     public int NumRef { get; set; }
     
     public int NbExemplaires { get; set; }
+
+    public bool Emprunte { get; set; } = false;
     
-    public bool Emprunte { get; set; }
+    public string? NomEmprunteur { get; set; }
     
-    public string NomEmprunteur { get; set; }
+    public string? PrenomEmprunteur { get; set; }
     
-    public string PrenomEmprunteur { get; set; }
+    public string? AdresseEmprunteur { get; set; }
     
-    public string AdresseEmprunteur { get; set; }
+    public string? TelEmprunteur { get; set; }
     
-    public string TelEmprunteur { get; set; }
+    public string? EmailEmprunteur { get; set; }
     
-    public string EmailEmprunteur { get; set; }
-    
-    public DateTime DateEmprunt { get; set; }
+    public DateTime? DateEmprunt { get; set; }
     
     
     public static Media operator +(Media media, int nbExemplaires)
--- Exercice 1/Library/Library.cs
+++ Exercice 1/Library/Library.cs
@@ -14,7 +14,7 @@ public class Library
     public void RemoveMedia(Media media)
     {
         Medias.Remove(media);
-    }
+    }   
     
     public Media this[int index]
     {
@@ -35,5 +35,92 @@ public class Library
             media.DateEmprunt = DateTime.Now;
             media.NbExemplaires--;
         }
+    }
+    
+    public void RendreMedia(Media media)
+    {
+        if (media.Emprunte)
+        {
+            media.Emprunte = false;
+            media.NomEmprunteur = null;
+            media.PrenomEmprunteur = null;
+            media.AdresseEmprunteur = null;
+            media.TelEmprunteur = null;
+            media.EmailEmprunteur = null;
+            media.DateEmprunt = null;
+            media.NbExemplaires++;
+        }
+    }
+    
+    public List<Media> RechercherMedia(string recherche)
+    {
+        List<Media> medias = new List<Media>();
+        
+        foreach (var media in Medias)
+        {
+            switch (media)
+            {
+                case CD:
+                    if (media.Titre.Contains(recherche) || ((CD) media).Auteur.Contains(recherche) || ((CD) media).Editeur.Contains(recherche))
+                    {
+                        medias.Add(media);
+                    }
+                    break;
+                case DVD:
+                    if (media.Titre.Contains(recherche) || ((DVD) media).Realisateur.Contains(recherche))
+                    {
+                        medias.Add(media);
+                    }
+                    break;
+                case Livre:
+                    if (media.Titre.Contains(recherche) || ((Livre) media).Auteur.Contains(recherche) || ((Livre) media).Editeur.Contains(recherche))
+                    {
+                        medias.Add(media);
+                    }
+                    break;
+                default:
+                    if(media.Titre.Contains(recherche))
+                        medias.Add(media);
+                    break;
+            }
+        }
+        return medias;
+    }
+    
+    public List<Media> RechercherMediaEmprunte(string nomEmprunteur, string prenomEmprunteur)
+    {
+        List<Media> medias = new List<Media>();
+        
+        foreach (var media in Medias)
+        {
+            if (media.NomEmprunteur == nomEmprunteur && media.PrenomEmprunteur == prenomEmprunteur)
+            {
+                medias.Add(media);
+            }
+        }
+        return medias;
+    }
+    public void AfficherStats()
+    {
+        int totalMedia = 0;
+        int totalMediaEmprunte = 0;
+        int totalMediaNonEmprunte = 0;
+        
+        foreach (var media in Medias)
+        {
+            totalMedia++;
+            if (media.Emprunte)
+            {
+                totalMediaEmprunte++;
+            }
+            else
+            {
+                totalMediaNonEmprunte++;
+            }
+        }
+        
+        Console.WriteLine($"Total de médias: {totalMedia}");
+        Console.WriteLine($"Total de médias empruntés: {totalMediaEmprunte}");
+        Console.WriteLine($"Total de médias non empruntés: {totalMediaNonEmprunte}");
     }
 }
